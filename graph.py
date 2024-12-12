c33e1147f4b53d30ec9b9d12901ce06872c88742
Shawn Thomas <shawnthomas0507@gmail.com>
2024-12-12 18:44:09 -0500

    created a google calendar node

--- graph.py
+++ graph.py
@@ -7,7 +7,7 @@ from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
 from langgraph.checkpoint.memory import MemorySaver
 from read_rag import qa_chain
 from rag import insert_rag
-from tools import rag_retrieval
+from tools import rag_retrieval,timetable_agent,calendar
 
 llm = ChatGroq(
     temperature=0,
@@ -45,15 +45,17 @@ def save_rag(state:MessagesState):
 
 def agent(state:MessagesState):
         user_query=state["messages"][-1]
-        ai_response2=llm.invoke([SystemMessage(content=f"You are an intelligent general assistant. You will be given a sentence. You have to tell whether the user query is an end of conversation sentence or not or if it is a resume related question or if it is a general question. If it is an end of conversation sentence just say True and if it is a resume related question just say resume and if it is a general question just say general. The sentence is {user_query}")])
+        ai_response2=llm.invoke([SystemMessage(content=f"You are an intelligent general assistant. You will be given a sentence. You have to tell whether the user query is an end of conversation sentence or not or if it is a resume related question or if it is a general question or it is a query where the user describes his/her day . If it is an end of conversation sentence just say True and if it is a resume related question just say resume and if it is a general question just say general and if it is a  query where the user describes his day say daily. The sentence is {user_query}")])
         if "resume" in ai_response2.content.lower():
              return {"messages":[AIMessage(content="rag_node")]}
+        elif "daily" in ai_response2.content.lower():
+            return {"messages":[AIMessage(content="timetable_node")]}
         elif "general" in ai_response2.content.lower():
-             sys_message=SystemMessage(content="You are a helpful assistant tasked with answering the user's questions.")
+             sys_message=SystemMessage(content="You are a helpful assistant tasked with answering the user's questions.If the user asks about creating a timetable or schedule ask him what does your todays schedule look like?")
              ai_response = llm.invoke([sys_message]+state["messages"])
              return {"messages": [AIMessage(content=ai_response.content)]}
         elif "true" in ai_response2.content.lower():
-             sys_message=SystemMessage(content="You are a helpful assistant tasked with answering the user's questions.")
+             sys_message=SystemMessage(content="You are a helpful assistant tasked with answering the user's questions.If the user asks about creating a timetable or schedule ask him what does your todays schedule look like?")
              ai_response = llm.invoke([sys_message]+state["messages"])
              return {"messages":[AIMessage(content=ai_response.content)],"messages": [AIMessage(content="END")]}
 
@@ -63,6 +65,8 @@ def should_continue(state:MessagesState):
         return END
       elif message.content=="rag_node":
         return "rag_node"
+      elif message.content=="timetable_node":
+          return "timetable_node"
       else:
         return "human_feedback"
 
@@ -75,12 +79,16 @@ graph.add_node("save_rag",save_rag)
 graph.add_node("path_to_pdf",path_to_pdf)
 graph.add_node("human_feedback",human_feedback)
 graph.add_node("rag_node",rag_retrieval)
+graph.add_node("timetable_node",timetable_agent)
+graph.add_node("calendar_node",calendar)
 graph.add_conditional_edges(START,check_rag_for_resume)
 graph.add_edge("path_to_pdf","save_rag")
 graph.add_edge("save_rag","human_feedback")
 graph.add_edge("human_feedback","agent")
-graph.add_conditional_edges("agent",should_continue,[END, "human_feedback","rag_node"])
+graph.add_conditional_edges("agent",should_continue,[END, "human_feedback","rag_node","timetable_node"])
 graph.add_edge("rag_node","human_feedback")
+graph.add_edge("timetable_node","calendar_node")
+graph.add_edge("calendar_node",END)
 memory=MemorySaver()
 app=graph.compile(checkpointer=memory)
 
--- tools.py
+++ tools.py
@@ -4,8 +4,11 @@ from langchain_groq import ChatGroq
 from classes import Info,Timetable
 from langchain_core.messages import HumanMessage,AIMessage,SystemMessage
 import streamlit as st
-from classes import MessagesState
+from classes import MessagesState,event
 from read_rag import qa_chain
+from gcsa.google_calendar import GoogleCalendar
+from datetime import datetime
+from gcsa.event import Event
 
 llm = ChatGroq(
     temperature=0,
@@ -13,51 +16,28 @@ llm = ChatGroq(
     model="llama-3.1-70b-versatile"
 )
 
-@tool
-def resume_helper(text):
-    
-    """
-    Argument: text
-    This tool is used to extract the skills from a extracted resume text.
-    """
+
+def timetable_agent(state:MessagesState):   
+    user_query=state["messages"][-2] 
     instructions="""
-    You are an intelligent assistant that is able to extract skills and projects from a resume.
-    You will be given an extracted resume text and you need to extract the skills and projects of the person from the extracted resume text.
-    You need to return the skills and projects in a structured manner. Also describe the projects in detail.
-    The resume is as follows: {text}
+    You are an assistant that reads in the user's text containing his daily schedule. You need to extract the time slots and the tasks to be done in the time slots from the text. You then need to print out the time slots and tasks in a structured manner that looks like a table.
+    The user's text is as follows: {text}.
+    Remember i dont want any python code. I just want you to extract the tasks and time slots and print them nicely.
     """
-    sys_message=instructions.format(text=text)
-    response=llm.with_structured_output(Info).invoke([SystemMessage(content=sys_message)])
-    return response.Information,{"skills_agent_output":response.Information}
-
-
-@tool
-def timetable_agent(text):
-    """
-    argument:text
-    This tool is used to create a daily timetable for the user based on the missing skills, tools and projects.
-    """
-
-
-    instructions="""
-    You are an assistant that reads in the user's text containing his daily schedule. You need to extract the time slots and the tasks to be done in the time slots from the text.
-    The user's text is as follows: {text}
-    """
-    user_skills=MessagesState["skills_agent_output"]
-    sys_message=instructions.format(text=text)
-    response=llm.with_structured_output(Timetable).invoke([SystemMessage(content=sys_message)])
-    user_timetable=response.to_dataframe()
+    sys_message=instructions.format(text=user_query)
+    response=llm.invoke([SystemMessage(content=sys_message)])
 
     instructions="""
     You are an intelligent assistant that is able to create a daily timetable for the user based on the missing skills, tools and projects.
     The user's daily timetable is as follows: {user_timetable}
-    The user's missing skills are as follows: {user_skills}.
     Your first task is to select one tool from the user's missing skills which he can learn in his free time.
-    You are to return the time slots and time slots of the user's daily schedule along with the new tool the user needs to learn.
+    You are to return the time slots and time slots of the user's daily schedule along with the new tool the user needs to learn in a structured manner that looks like a table.
+    Remember, you are to only give only one day's schedule not for the whole week. And you should be more specific on what exactly you can learn about that tool in the specific time slot.
+    Also once you give the schedule inform the user that you will be adding it to the calendar.
     """
-    sys_message=instructions.format(user_timetable=user_timetable,user_skills=user_skills)
-    response1=llm.with_structured_output(Timetable).invoke([SystemMessage(content=sys_message)])
-    return response1.to_dataframe()
+    sys_message=instructions.format(user_timetable=response.content)
+    response1=llm.invoke(state["messages"]+[SystemMessage(content=sys_message)])
+    return {"messages":response1.content}
 
 
 def rag_retrieval(state:MessagesState):
@@ -72,6 +52,52 @@ def rag_retrieval(state:MessagesState):
     sys_message=instructions.format(user_query=user_query,context=response)
     response=llm.invoke([SystemMessage(content=sys_message)])
     return {"messages":response.content}
+
+
+
+def calendar(state:MessagesState):
+    tool=state["messages"][-1]
+    instructions="""
+    You are an intelligent assistant. You will be given a sentence. 
+    You should extract the tool to be learnt from it and the time the tool will be learnt. Also the location to learn this tool is philadelphia.
+    Also you will need todays date and time. todays date and time: {today} 
+    text: {text}
+    Using the tool and time you need to create a event function.
+
+    I want the output to be in this format:
+    event=Event(
+    'The Glass Menagerie',
+    start=datetime(2020, 7, 10, 19, 0),
+    location='Záhřebská 468/21',
+    minutes_before_popup_reminder=15
+    )
+
+    This format should be strictly followed. Dont output any code or any sort of text just this Event function. Be very mindful of the indentation it should be perfect.
+    """
+    sys_message=instructions.format(text=tool,today=datetime.now())
+    response=llm.invoke([SystemMessage(content=sys_message)])
+    
+
+    str1="""
+    from gcsa.event import Event
+    from gcsa.google_calendar import GoogleCalendar
+    from datetime import datetime
+    from gcsa.event import Event
+
+    calendar = GoogleCalendar('shawnthomas0507@gmail.com')
+    """
+
+    str2=response.content
+    if not str2.startswith("event="):
+         str2 = "event=" + str2
+
+    str3="\ncalendar.add_event(event)"
+
+    str1+=str2
+    str1+=str3
+    event_str = "\n".join([line.strip() for line in str1.splitlines()])
+    exec(event_str)
+    return {"messages":" I have successfully created an event for you in google calendar to study this skill. Good Luck !"}
 
 
 
